890c8abd92efc4d0b9a10ec3fb371476ffcdcd06
Anand Angalig <aangalig@delphicdigital.com>
2018-05-02 17:58:09 -0400

    initialized search bar state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // ===================== Functional Component: no state awareness
 // const SearchBar = () => {
 //   return <input />; // this will tunr into React.creatComponent , so we had to import React above
@@ -6,15 +6,24 @@ import React, {Component} from 'react';
 
 // ===================== Class Bases Component: state awareness + additional functionalities
 class SearchBar extends Component{
-  render() {
-    return <input onChange={this.onInputChange} />; // onChange is a protected React prop; Adding the event listener
-    // this line can be shortened using arrow function: onInputChange() can be deleted
-    // return <input onChange={event => console.log(event.target.value)} />;
+  constructor(props){ // the only automatically called method of a class that gets called with each instantiation of the class
+    super(props); // super method talks to the parent class, in this case its Component
+
+    this.state = { term: '' }; // here, we are creting an empty object and assigning it to the state.
+    //STATE: a plain JS object that records and reacts to user events!
+    // All class based components come with the STATE => re-renders iteself and its' children when state changes
   }
 
-  //the event handling function:
-  onInputChange(event){
-    console.log(event.target.value);
+  render() {
+    // onChange is a protected React prop; Adding the event listener
+    // this line can be shortened using arrow function: onInputChange() can be deleted
+    // return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value input: {this.state.term}
+      </div>
+    );
   }
 }
 export default SearchBar; // whenever other components import this silo-ed file/component, it will have get this
